fcb7b4d7c76deb0193407e9cee7eae2c1e32a6fe
Mudasir Zahoor <mudassirzahoor14@gmail.com>
2024-12-30 01:46:42 +0530

    fix loader

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loader: React.FC = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-100 bg-opacity-75 z-50">
-      <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"></div>
+      <div className="animate-spin rounded-full border-4 border-t-4 border-t-white border-gray-600 h-32 w-32"></div>
     </div>
   );
 };
